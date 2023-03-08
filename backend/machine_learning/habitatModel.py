29ba3a8b0a03f3b30120637b5ee7d3d200a592f9
Emanuele Mezzi <manuzzi99@gmail.com>
2023-03-08 18:00:45 +0100

    main modifications.

---
+++ backend/machine_learning/habitatModel.py
@@ -0,0 +1,29 @@
+import joblib
+import pandas as pd
+
+
+class HabitatModel:
+    version = None
+    classifier = None
+    dataset: pd.DataFrame = None
+
+    def __init__(self, version: str = "latest"):
+        self.version = version
+        if version == "latest":
+            try:
+                # Load latest model and data
+                self.classifier = joblib.load('machine_learning/model_data/habitat_random_forest_model.joblib')
+                self.dataset = pd.read_csv("../../datasets/habitat/Habitat_Stress.csv")
+            except Exception as e:
+                print("Error when loading data and model: ", e)
+        else:
+            raise Exception("No other models saved for this task!")
+
+    def predict(self, water_data: pd.DataFrame):
+        return self.classifier.predict(water_data)
+
+    def get_accuracy_metrics(self):
+        pass
+
+    def get_water_data(self, limit: int = 10, offset: int = 0):
+        return self.dataset.iloc[::-1].iloc[offset:limit]
---
+++ backend/machine_learning/radiationExpositionModel.py
@@ -0,0 +1,27 @@
+import joblib
+import json
+import numpy as np
+from sklearn import metrics
+import pandas as pd
+
+
+class RadiationExpositionModel:
+    def __init__(self, version: str = 'latest'):
+        self.version = version
+
+        if version == 'latest':
+            try:
+                self.classifier = joblib.load('machine_learning/model_data/radiation_exposition_model.pkl')
+            except Exception as e:
+                print("Error when loading data and model: ", e)
+        else:
+            raise Exception("No other models saved for this task!")
+
+        def predict(self, genes_data: pd.DataFrame):
+            return self.classifier.predict(genes_data)
+
+        def get_accuracy_metrics(self):
+            pass
+
+        def get_genes_data(self):
+            pass
